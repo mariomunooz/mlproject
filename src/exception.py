e4121e235d5e7ca05366cd8a9e9cb50f7a1c0326
Mario Muñoz <mario.munoz01@estudiant.upf.edu>
2024-09-22 16:16:07 +0200

    EDA and Model training notebooks

--- src/exception.py
+++ src/exception.py
@@ -1,10 +1,10 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = f"Error ocurred in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+    error_message = f"Error occurred in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
     
     return error_message
     
@@ -16,5 +16,14 @@ class CustomException(Exception):
         
     def __str__(self):
         return self.error_message
+    
+    
+#if __name__ == '__main__':
+#    try:
+#        a = 1 / 0
+#    except Exception as e:  # Catch the actual exception type
+#        logging.error("A division by zero error occurred")  # Log with error level and include exception info
+#        raise CustomException(e, sys)  # Raise the CustomException with details
+        
     
     
